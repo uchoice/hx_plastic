d66030f49e13aab5f429429123c99ca04cda7bd8
coderPXin <582088281@qq.com>
2019-06-15 11:07:46 +0800

    1.0.7

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -14,7 +14,7 @@ export default function () {
         <p>成都桦萱医疗美容整形医院</p>
         <p>联系电话：028-87365575</p>
         <p>地址：成都市锦江区宏济上路140号</p>
-        <p><a href="http://www.hsuanmr.com/admin.html">桦萱(hsuan) 蜀ICP备19015805号-1</a></p>
+        <p><a href="http://www.hsuanmr.com/admin.html#/user/login">桦萱(hsuan) 蜀ICP备19015805号-1</a></p>
       </div>
       <div className="qrcode-wrapper">
         <img alt="" src={QR_CODE_IMG}/>
